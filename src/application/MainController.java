538db05027dc4b82d18d67b80660aec8914691f4
petrep <petrep@freemail.hu>
2017-12-03 15:38:23 +0100

    basic functionality added, hardcoded

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,6 +1,10 @@
 package application;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 
@@ -8,10 +12,37 @@ import javafx.scene.control.TextArea;
 public class MainController {
 	@FXML
 	TextArea TextArea;
-	public void Search(){
+	public void Search() throws FileNotFoundException{
 		System.out.println("MainController starting");
 		TextArea.setText("executing MainController");
 		
+		String searchedExpression = "BinaryDecoder";
+		
+		File[] files = new File("c:\\liferay\\canbedeleted\\").listFiles();
+		searchFiles(files, searchedExpression);
+		
+		
+
+	}
+	private void searchFiles(File[] files, String searchedExpression) throws FileNotFoundException {
+		for (File file : files) {
+	        if (file.isDirectory()) {
+	            System.out.println("Directory: " + file.getName());
+	            searchFiles(file.listFiles(), searchedExpression); // Calls same method again.
+	        } else {
+	            System.out.println("File: " + file.getName());
+	            
+	            Scanner scanner = new Scanner(file);
+	    		while (scanner.hasNextLine()) {
+	    		   final String lineFromFile = scanner.nextLine();
+	    		   if(lineFromFile.contains(searchedExpression)) { 
+	    		       // a match!
+	    		       System.out.println("I found " +searchedExpression+ " in file " +file.getName());
+	    		       break;
+	    		   }
+	    		}
+	        }
+	    }
 		
 		
 	}
